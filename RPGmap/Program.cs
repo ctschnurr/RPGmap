59e323d2ee4fd5f6c08d3a91d16163b3c0aebc08
ctschnurr <113189402+ctschnurr@users.noreply.github.com>
2022-11-08 10:48:41 -0400

    Got scale working great
    also added frame, just about ready to hand in just needs a little more polish

--- RPGmap/Program.cs
+++ RPGmap/Program.cs
@@ -27,18 +27,25 @@ namespace RPGmap
         static void Main(string[] args)
 
         {
-            //Console.SetWindowSize(62, 26);
-            //Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+            Console.SetWindowSize(92, 39);
+            Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
 
             Console.CursorVisible = false;
+
+            DisplayMap();
+
+            Console.ReadKey(true);
+            Console.Clear();
 
             DisplayMap(1);
 
             Console.ReadKey(true);
+            Console.Clear();
 
             DisplayMap(2);
 
             Console.ReadKey(true);
+            Console.Clear();
 
             DisplayMap(3);
 
@@ -70,53 +77,97 @@ namespace RPGmap
         {
 
             int mapV = 0;
-            int drawX = 0;
-            int gridX = 0;
+            int drawV;
 
             int mapH = 0;
-            int drawY = 0;
 
-            for (int z = 0; z < 30; z++)
+            int rowNum = 1;
+            int colNum = 1;
+
+            switch (scale)
             {
-                
-                for (int x = 0; x < scale; x++)
-                {
-                    char tile = (map[mapV, mapH]);
+                case 1:
+                    rowNum = 12;
+                    colNum = 30;
+                    break;
 
-                    Console.SetCursorPosition(drawY, drawX);
-                    for (int y = 0; y < scale; y++)
-                    {
-                        Console.Write(tile);
-                    }
-                    drawX = drawX + 1;
-                }
-                mapY = mapY + 1;
+                case 2:
+                    rowNum = 6;
+                    colNum = 15;
+                    break;
 
+                case 3:
+                    rowNum = 0;
+                    colNum = 0;
+                    break;
             }
 
-            
+            int mapRow = rowNum + 1;
+            int mapCol = colNum + 1;
 
+            int drawH = mapCol;
 
+            for (int column = 0; column < 12; column++)
+            {
+                for (int row = 0; row < 30; row++)
+                {
+                    drawV = mapRow;
+                    for (int v = 0; v < scale; v++)
+                    {
+                        char tile = (map[mapV, mapH]);
 
+                        Console.SetCursorPosition(drawH, drawV);
+                        for (int h = 0; h < scale; h++)
+                        {
+                            Console.Write(tile);
+                        }
+                        drawV = drawV + 1;
+                    }
+                    mapH = mapH + 1;
+                    drawH = drawH + scale;
+                    
 
-            //      for (int mapX = 0; mapX < 12; mapX++)
-            //      {
-            //          Console.SetCursorPosition(0, mapX);
-            //          for (int mapY = 0; mapY < 30; mapY++)
-            //          {
-            //              char tile = (map[mapX, mapY]);
-            //
-            //              for (int x = 1; x <= scale; x++)
-            //              {
-            //                  for(int y = 1; y <= scale; y++)
-            //                  {
-            //                      Console.Write(tile);
-            //                  }
-            //                  Console.SetCursorPosition(Console.CursorLeft - scale, Console.CursorTop + x);
-            //              }
-            //          }
-            //
-            //      }
+                }
+                mapRow = mapRow + scale;
+                drawH = mapCol;
+                
+                mapV = mapV + 1;
+                mapH = 0;          
+            }
+
+            Console.SetCursorPosition(colNum, rowNum);
+
+            Console.Write("╔");
+
+            for (int x = 0; x < (30 * scale); x++)
+            {
+                Console.Write("═");
+            }
+
+            Console.WriteLine("╗");
+
+            for (int y = 1; y <= (12 * scale); y++)
+            {
+                Console.SetCursorPosition(colNum, rowNum + y);
+                Console.Write("║");
+                Console.SetCursorPosition(colNum + ((30 * scale) + 1), rowNum + y);
+                Console.Write("║");
+            }
+
+            Console.SetCursorPosition(colNum, mapRow);
+
+            Console.Write("╚");
+
+            for (int x = 0; x < (30 * scale); x++)
+            {
+                Console.Write("═");
+            }
+
+            Console.Write("╝");
+
+            Console.SetCursorPosition(colNum, mapRow + 1);
+
+            Console.Write(" Scale: " + scale);
 
         }
     }
